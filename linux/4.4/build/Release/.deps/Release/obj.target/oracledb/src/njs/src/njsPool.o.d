145c89edc6109d7b4622f2d76987ca8c9342f89c
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2016-03-16 21:59:57 +0600

    linux 4.4

---
+++ linux/4.4/build/Release/.deps/Release/obj.target/oracledb/src/njs/src/njsPool.o.d
@@ -0,0 +1,111 @@
+cmd_Release/obj.target/oracledb/src/njs/src/njsPool.o := g++ '-DNODE_GYP_MODULE_NAME=oracledb' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/root/.node-gyp/4.4.0/include/node -I/root/.node-gyp/4.4.0/src -I/root/.node-gyp/4.4.0/deps/uv/include -I/root/.node-gyp/4.4.0/deps/v8/include -I/opt/collectorjs/instantclient/sdk/include -I../src/dpi/src -I../src/dpi/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fexceptions -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -fexceptions -MMD -MF ./Release/.deps/Release/obj.target/oracledb/src/njs/src/njsPool.o.d.raw   -c -o Release/obj.target/oracledb/src/njs/src/njsPool.o ../src/njs/src/njsPool.cpp
+Release/obj.target/oracledb/src/njs/src/njsPool.o: \
+ ../src/njs/src/njsPool.cpp /root/.node-gyp/4.4.0/include/node/node.h \
+ /root/.node-gyp/4.4.0/include/node/v8.h \
+ /root/.node-gyp/4.4.0/include/node/v8-version.h \
+ /root/.node-gyp/4.4.0/include/node/v8config.h \
+ /root/.node-gyp/4.4.0/include/node/node_version.h \
+ ../src/njs/src/njsOracle.h /root/.node-gyp/4.4.0/include/node/v8.h \
+ ../node_modules/nan/nan.h \
+ /root/.node-gyp/4.4.0/include/node/node_version.h \
+ /root/.node-gyp/4.4.0/include/node/uv.h \
+ /root/.node-gyp/4.4.0/include/node/uv-errno.h \
+ /root/.node-gyp/4.4.0/include/node/uv-version.h \
+ /root/.node-gyp/4.4.0/include/node/uv-unix.h \
+ /root/.node-gyp/4.4.0/include/node/uv-threadpool.h \
+ /root/.node-gyp/4.4.0/include/node/uv-linux.h \
+ /root/.node-gyp/4.4.0/include/node/node_buffer.h \
+ /root/.node-gyp/4.4.0/include/node/node.h \
+ /root/.node-gyp/4.4.0/include/node/node_object_wrap.h \
+ ../node_modules/nan/nan_callbacks.h \
+ ../node_modules/nan/nan_callbacks_12_inl.h \
+ ../node_modules/nan/nan_maybe_43_inl.h \
+ ../node_modules/nan/nan_converters.h \
+ ../node_modules/nan/nan_converters_43_inl.h \
+ ../node_modules/nan/nan_new.h \
+ ../node_modules/nan/nan_implementation_12_inl.h \
+ ../node_modules/nan/nan_persistent_12_inl.h \
+ ../node_modules/nan/nan_weak.h ../node_modules/nan/nan_object_wrap.h \
+ ../node_modules/nan/nan_typedarray_contents.h ../src/dpi/include/dpi.h \
+ ../src/dpi/include/dpiException.h ../src/dpi/include/dpiEnv.h \
+ ../src/dpi/include/dpiCommon.h ../src/dpi/include/dpiPool.h \
+ ../src/dpi/include/dpiConn.h ../src/dpi/include/dpiLob.h \
+ ../src/dpi/include/dpiStmt.h \
+ /opt/collectorjs/instantclient/sdk/include/oci.h \
+ /opt/collectorjs/instantclient/sdk/include/oratypes.h \
+ /opt/collectorjs/instantclient/sdk/include/ocidfn.h \
+ /opt/collectorjs/instantclient/sdk/include/oci1.h \
+ /opt/collectorjs/instantclient/sdk/include/oro.h \
+ /opt/collectorjs/instantclient/sdk/include/ori.h \
+ /opt/collectorjs/instantclient/sdk/include/ort.h \
+ /opt/collectorjs/instantclient/sdk/include/orl.h \
+ /opt/collectorjs/instantclient/sdk/include/ociextp.h \
+ /opt/collectorjs/instantclient/sdk/include/ociapr.h \
+ /opt/collectorjs/instantclient/sdk/include/ociap.h \
+ /opt/collectorjs/instantclient/sdk/include/nzt.h \
+ /opt/collectorjs/instantclient/sdk/include/nzerror.h \
+ /opt/collectorjs/instantclient/sdk/include/ocixmldb.h \
+ /opt/collectorjs/instantclient/sdk/include/oci8dp.h \
+ /opt/collectorjs/instantclient/sdk/include/ocixstream.h \
+ ../src/dpi/include/dpiDateTimeArray.h ../src/njs/src/njsUtils.h \
+ ../src/njs/src/njsMessages.h ../src/njs/src/njsPool.h \
+ ../src/njs/src/njsConnection.h
+../src/njs/src/njsPool.cpp:
+/root/.node-gyp/4.4.0/include/node/node.h:
+/root/.node-gyp/4.4.0/include/node/v8.h:
+/root/.node-gyp/4.4.0/include/node/v8-version.h:
+/root/.node-gyp/4.4.0/include/node/v8config.h:
+/root/.node-gyp/4.4.0/include/node/node_version.h:
+../src/njs/src/njsOracle.h:
+/root/.node-gyp/4.4.0/include/node/v8.h:
+../node_modules/nan/nan.h:
+/root/.node-gyp/4.4.0/include/node/node_version.h:
+/root/.node-gyp/4.4.0/include/node/uv.h:
+/root/.node-gyp/4.4.0/include/node/uv-errno.h:
+/root/.node-gyp/4.4.0/include/node/uv-version.h:
+/root/.node-gyp/4.4.0/include/node/uv-unix.h:
+/root/.node-gyp/4.4.0/include/node/uv-threadpool.h:
+/root/.node-gyp/4.4.0/include/node/uv-linux.h:
+/root/.node-gyp/4.4.0/include/node/node_buffer.h:
+/root/.node-gyp/4.4.0/include/node/node.h:
+/root/.node-gyp/4.4.0/include/node/node_object_wrap.h:
+../node_modules/nan/nan_callbacks.h:
+../node_modules/nan/nan_callbacks_12_inl.h:
+../node_modules/nan/nan_maybe_43_inl.h:
+../node_modules/nan/nan_converters.h:
+../node_modules/nan/nan_converters_43_inl.h:
+../node_modules/nan/nan_new.h:
+../node_modules/nan/nan_implementation_12_inl.h:
+../node_modules/nan/nan_persistent_12_inl.h:
+../node_modules/nan/nan_weak.h:
+../node_modules/nan/nan_object_wrap.h:
+../node_modules/nan/nan_typedarray_contents.h:
+../src/dpi/include/dpi.h:
+../src/dpi/include/dpiException.h:
+../src/dpi/include/dpiEnv.h:
+../src/dpi/include/dpiCommon.h:
+../src/dpi/include/dpiPool.h:
+../src/dpi/include/dpiConn.h:
+../src/dpi/include/dpiLob.h:
+../src/dpi/include/dpiStmt.h:
+/opt/collectorjs/instantclient/sdk/include/oci.h:
+/opt/collectorjs/instantclient/sdk/include/oratypes.h:
+/opt/collectorjs/instantclient/sdk/include/ocidfn.h:
+/opt/collectorjs/instantclient/sdk/include/oci1.h:
+/opt/collectorjs/instantclient/sdk/include/oro.h:
+/opt/collectorjs/instantclient/sdk/include/ori.h:
+/opt/collectorjs/instantclient/sdk/include/ort.h:
+/opt/collectorjs/instantclient/sdk/include/orl.h:
+/opt/collectorjs/instantclient/sdk/include/ociextp.h:
+/opt/collectorjs/instantclient/sdk/include/ociapr.h:
+/opt/collectorjs/instantclient/sdk/include/ociap.h:
+/opt/collectorjs/instantclient/sdk/include/nzt.h:
+/opt/collectorjs/instantclient/sdk/include/nzerror.h:
+/opt/collectorjs/instantclient/sdk/include/ocixmldb.h:
+/opt/collectorjs/instantclient/sdk/include/oci8dp.h:
+/opt/collectorjs/instantclient/sdk/include/ocixstream.h:
+../src/dpi/include/dpiDateTimeArray.h:
+../src/njs/src/njsUtils.h:
+../src/njs/src/njsMessages.h:
+../src/njs/src/njsPool.h:
+../src/njs/src/njsConnection.h:
