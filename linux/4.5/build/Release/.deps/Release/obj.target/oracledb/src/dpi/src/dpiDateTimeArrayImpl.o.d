15c0a20826dc76132f57b5db41cb4b84be7ff95f
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2016-08-26 14:07:33 +0700

    linux 4.5 build

---
+++ linux/4.5/build/Release/.deps/Release/obj.target/oracledb/src/dpi/src/dpiDateTimeArrayImpl.o.d
@@ -0,0 +1,52 @@
+cmd_Release/obj.target/oracledb/src/dpi/src/dpiDateTimeArrayImpl.o := g++ '-DNODE_GYP_MODULE_NAME=oracledb' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/root/.node-gyp/4.5.0/include/node -I/root/.node-gyp/4.5.0/src -I/root/.node-gyp/4.5.0/deps/uv/include -I/root/.node-gyp/4.5.0/deps/v8/include -I/opt/collectorjs/instantclient -I../src/dpi/src -I../src/dpi/include -I../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fexceptions -O3 -ffunction-sections -fdata-sections -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -fexceptions -MMD -MF ./Release/.deps/Release/obj.target/oracledb/src/dpi/src/dpiDateTimeArrayImpl.o.d.raw   -c -o Release/obj.target/oracledb/src/dpi/src/dpiDateTimeArrayImpl.o ../src/dpi/src/dpiDateTimeArrayImpl.cpp
+Release/obj.target/oracledb/src/dpi/src/dpiDateTimeArrayImpl.o: \
+ ../src/dpi/src/dpiDateTimeArrayImpl.cpp ../src/dpi/src/dpiUtils.h \
+ /opt/collectorjs/instantclient/oci.h \
+ /opt/collectorjs/instantclient/oratypes.h \
+ /opt/collectorjs/instantclient/ocidfn.h \
+ /opt/collectorjs/instantclient/oci1.h \
+ /opt/collectorjs/instantclient/oro.h \
+ /opt/collectorjs/instantclient/ori.h \
+ /opt/collectorjs/instantclient/ort.h \
+ /opt/collectorjs/instantclient/orl.h \
+ /opt/collectorjs/instantclient/ociextp.h \
+ /opt/collectorjs/instantclient/ociapr.h \
+ /opt/collectorjs/instantclient/ociap.h \
+ /opt/collectorjs/instantclient/nzt.h \
+ /opt/collectorjs/instantclient/nzerror.h \
+ /opt/collectorjs/instantclient/ocixmldb.h \
+ /opt/collectorjs/instantclient/oci8dp.h \
+ /opt/collectorjs/instantclient/ocixstream.h \
+ ../src/dpi/src/dpiDateTimeArrayImpl.h ../src/dpi/include/dpiEnv.h \
+ ../src/dpi/include/dpiCommon.h ../src/dpi/include/dpiPool.h \
+ ../src/dpi/include/dpiConn.h ../src/dpi/include/dpiLob.h \
+ ../src/dpi/include/dpiStmt.h ../src/dpi/include/dpiDateTimeArray.h \
+ ../src/dpi/src/dpiExceptionImpl.h ../src/dpi/include/dpiException.h
+../src/dpi/src/dpiDateTimeArrayImpl.cpp:
+../src/dpi/src/dpiUtils.h:
+/opt/collectorjs/instantclient/oci.h:
+/opt/collectorjs/instantclient/oratypes.h:
+/opt/collectorjs/instantclient/ocidfn.h:
+/opt/collectorjs/instantclient/oci1.h:
+/opt/collectorjs/instantclient/oro.h:
+/opt/collectorjs/instantclient/ori.h:
+/opt/collectorjs/instantclient/ort.h:
+/opt/collectorjs/instantclient/orl.h:
+/opt/collectorjs/instantclient/ociextp.h:
+/opt/collectorjs/instantclient/ociapr.h:
+/opt/collectorjs/instantclient/ociap.h:
+/opt/collectorjs/instantclient/nzt.h:
+/opt/collectorjs/instantclient/nzerror.h:
+/opt/collectorjs/instantclient/ocixmldb.h:
+/opt/collectorjs/instantclient/oci8dp.h:
+/opt/collectorjs/instantclient/ocixstream.h:
+../src/dpi/src/dpiDateTimeArrayImpl.h:
+../src/dpi/include/dpiEnv.h:
+../src/dpi/include/dpiCommon.h:
+../src/dpi/include/dpiPool.h:
+../src/dpi/include/dpiConn.h:
+../src/dpi/include/dpiLob.h:
+../src/dpi/include/dpiStmt.h:
+../src/dpi/include/dpiDateTimeArray.h:
+../src/dpi/src/dpiExceptionImpl.h:
+../src/dpi/include/dpiException.h:
