ecfe2377a122d9675c2cc01947234b6bd2fb0049
Nickolay Alexandrov <nick.alexandrov@gmail.com>
2017-01-23 15:11:17 +0700

    6.9

---
+++ linux/6.9/build/Release/.deps/Release/obj.target/oracledb/src/njs/src/njsConnection.o.d
@@ -0,0 +1,106 @@
+cmd_Release/obj.target/oracledb/src/njs/src/njsConnection.o := g++ '-DNODE_GYP_MODULE_NAME=oracledb' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/root/.node-gyp/6.9.4/include/node -I/root/.node-gyp/6.9.4/src -I/root/.node-gyp/6.9.4/deps/uv/include -I/root/.node-gyp/6.9.4/deps/v8/include -I/opt/collectorjs/instantclient -I../src/dpi/src -I../src/dpi/include -I../../nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fexceptions -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -fexceptions -MMD -MF ./Release/.deps/Release/obj.target/oracledb/src/njs/src/njsConnection.o.d.raw   -c -o Release/obj.target/oracledb/src/njs/src/njsConnection.o ../src/njs/src/njsConnection.cpp
+Release/obj.target/oracledb/src/njs/src/njsConnection.o: \
+ ../src/njs/src/njsConnection.cpp ../src/njs/src/njsConnection.h \
+ /root/.node-gyp/6.9.4/include/node/node.h \
+ /root/.node-gyp/6.9.4/include/node/v8.h \
+ /root/.node-gyp/6.9.4/include/node/v8-version.h \
+ /root/.node-gyp/6.9.4/include/node/v8config.h \
+ /root/.node-gyp/6.9.4/include/node/node_version.h \
+ ../src/dpi/include/dpi.h ../src/dpi/include/dpiException.h \
+ ../src/dpi/include/dpiEnv.h ../src/dpi/include/dpiCommon.h \
+ ../src/dpi/include/dpiPool.h ../src/dpi/include/dpiConn.h \
+ ../src/dpi/include/dpiLob.h /opt/collectorjs/instantclient/oci.h \
+ /opt/collectorjs/instantclient/oratypes.h \
+ /opt/collectorjs/instantclient/ocidfn.h \
+ /opt/collectorjs/instantclient/oci1.h \
+ /opt/collectorjs/instantclient/oro.h \
+ /opt/collectorjs/instantclient/ori.h \
+ /opt/collectorjs/instantclient/ort.h \
+ /opt/collectorjs/instantclient/orl.h \
+ /opt/collectorjs/instantclient/ociextp.h \
+ /opt/collectorjs/instantclient/ociapr.h \
+ /opt/collectorjs/instantclient/ociap.h \
+ /opt/collectorjs/instantclient/nzt.h \
+ /opt/collectorjs/instantclient/nzerror.h \
+ /opt/collectorjs/instantclient/ocixmldb.h \
+ /opt/collectorjs/instantclient/oci8dp.h \
+ /opt/collectorjs/instantclient/ocixstream.h ../src/dpi/include/dpiStmt.h \
+ ../src/dpi/include/dpiDateTimeArray.h ../src/njs/src/njsUtils.h \
+ ../../nan/nan.h /root/.node-gyp/6.9.4/include/node/node_version.h \
+ /root/.node-gyp/6.9.4/include/node/uv.h \
+ /root/.node-gyp/6.9.4/include/node/uv-errno.h \
+ /root/.node-gyp/6.9.4/include/node/uv-version.h \
+ /root/.node-gyp/6.9.4/include/node/uv-unix.h \
+ /root/.node-gyp/6.9.4/include/node/uv-threadpool.h \
+ /root/.node-gyp/6.9.4/include/node/uv-linux.h \
+ /root/.node-gyp/6.9.4/include/node/node_buffer.h \
+ /root/.node-gyp/6.9.4/include/node/node.h \
+ /root/.node-gyp/6.9.4/include/node/node_object_wrap.h \
+ ../../nan/nan_callbacks.h ../../nan/nan_callbacks_12_inl.h \
+ ../../nan/nan_maybe_43_inl.h ../../nan/nan_converters.h \
+ ../../nan/nan_converters_43_inl.h ../../nan/nan_new.h \
+ ../../nan/nan_implementation_12_inl.h ../../nan/nan_persistent_12_inl.h \
+ ../../nan/nan_weak.h ../../nan/nan_object_wrap.h \
+ ../../nan/nan_typedarray_contents.h ../src/njs/src/njsMessages.h \
+ ../src/njs/src/njsOracle.h /root/.node-gyp/6.9.4/include/node/v8.h \
+ ../src/njs/src/njsResultSet.h ../src/njs/src/njsIntLob.h
+../src/njs/src/njsConnection.cpp:
+../src/njs/src/njsConnection.h:
+/root/.node-gyp/6.9.4/include/node/node.h:
+/root/.node-gyp/6.9.4/include/node/v8.h:
+/root/.node-gyp/6.9.4/include/node/v8-version.h:
+/root/.node-gyp/6.9.4/include/node/v8config.h:
+/root/.node-gyp/6.9.4/include/node/node_version.h:
+../src/dpi/include/dpi.h:
+../src/dpi/include/dpiException.h:
+../src/dpi/include/dpiEnv.h:
+../src/dpi/include/dpiCommon.h:
+../src/dpi/include/dpiPool.h:
+../src/dpi/include/dpiConn.h:
+../src/dpi/include/dpiLob.h:
+/opt/collectorjs/instantclient/oci.h:
+/opt/collectorjs/instantclient/oratypes.h:
+/opt/collectorjs/instantclient/ocidfn.h:
+/opt/collectorjs/instantclient/oci1.h:
+/opt/collectorjs/instantclient/oro.h:
+/opt/collectorjs/instantclient/ori.h:
+/opt/collectorjs/instantclient/ort.h:
+/opt/collectorjs/instantclient/orl.h:
+/opt/collectorjs/instantclient/ociextp.h:
+/opt/collectorjs/instantclient/ociapr.h:
+/opt/collectorjs/instantclient/ociap.h:
+/opt/collectorjs/instantclient/nzt.h:
+/opt/collectorjs/instantclient/nzerror.h:
+/opt/collectorjs/instantclient/ocixmldb.h:
+/opt/collectorjs/instantclient/oci8dp.h:
+/opt/collectorjs/instantclient/ocixstream.h:
+../src/dpi/include/dpiStmt.h:
+../src/dpi/include/dpiDateTimeArray.h:
+../src/njs/src/njsUtils.h:
+../../nan/nan.h:
+/root/.node-gyp/6.9.4/include/node/node_version.h:
+/root/.node-gyp/6.9.4/include/node/uv.h:
+/root/.node-gyp/6.9.4/include/node/uv-errno.h:
+/root/.node-gyp/6.9.4/include/node/uv-version.h:
+/root/.node-gyp/6.9.4/include/node/uv-unix.h:
+/root/.node-gyp/6.9.4/include/node/uv-threadpool.h:
+/root/.node-gyp/6.9.4/include/node/uv-linux.h:
+/root/.node-gyp/6.9.4/include/node/node_buffer.h:
+/root/.node-gyp/6.9.4/include/node/node.h:
+/root/.node-gyp/6.9.4/include/node/node_object_wrap.h:
+../../nan/nan_callbacks.h:
+../../nan/nan_callbacks_12_inl.h:
+../../nan/nan_maybe_43_inl.h:
+../../nan/nan_converters.h:
+../../nan/nan_converters_43_inl.h:
+../../nan/nan_new.h:
+../../nan/nan_implementation_12_inl.h:
+../../nan/nan_persistent_12_inl.h:
+../../nan/nan_weak.h:
+../../nan/nan_object_wrap.h:
+../../nan/nan_typedarray_contents.h:
+../src/njs/src/njsMessages.h:
+../src/njs/src/njsOracle.h:
+/root/.node-gyp/6.9.4/include/node/v8.h:
+../src/njs/src/njsResultSet.h:
+../src/njs/src/njsIntLob.h:
